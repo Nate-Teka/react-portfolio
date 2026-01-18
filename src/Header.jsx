8d6c05afacc3ee9ec41060d7f1f87ded8400247b
Nate <nathanieltekalgn@gmail.com>
2026-01-18 22:41:04 +0300

    I dont even its been so long so.... here ya go ig

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,4 @@
-const pixelRatio = ()=>{
-    alert(window.devicePixelRatio);
-}
+
 function Header() {
     return (
         <>
@@ -10,12 +8,14 @@ function Header() {
                           <div className="navbar__user-info__container__img-container">
                           <img src="/placeholder.jpg" alt="Profile picture"/>
                         </div>
-                        <p className="navbar__user-info__container__name" onClick={pixelRatio}>Nathaniel Tekalgn</p>
+                        <p className="navbar__user-info__container__name">Nathaniel Tekalgn</p>
                     </div>
                     <ul className="navbar__options">
                         <li className="navbar__item"><a href="#aboutLink" className="navbar__item__link">about</a></li>
                         <li className="navbar__item" id="projectsLink"><a href="#" className="navbar__item__link">projects</a></li>
                         <li className="navbar__item" ><a href="#contactLink" className="navbar__item__link">contact me</a></li>
+                        <li className="navbar__item" ><a href="#contactLink" className="navbar__item__link">contact me</a></li>
+                        <li><button className="fa-solid fa-bars"></button></li>
                     </ul>
                 </nav>
         </header>
